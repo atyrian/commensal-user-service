14a15f4750893ac863b130ca8bf797a1c63c990e
Victor <victor.ricart@outlook.com>
2019-01-27 19:04:26 +0100

    Add unit tests

--- test/domain/Entity.spec.js
+++ test/domain/Entity.spec.js
@@ -10,94 +10,94 @@ const UserRepository = require('../../src/repositories/UserRepository');
 const BaseRepository = require('../../src/repositories/BaseRepository');
 
 describe('tests for Entity.js', function () {
-    beforeEach(function () {
-        this._exists = true;
-    });
+  beforeEach(function () {
+    this._exists = true;
+  });
 
-    afterEach(function () {
-        sandbox.restore();
-    });
+  afterEach(function () {
+    sandbox.restore();
+  });
 
-    describe('load()', function () {
-        it('resolves a promise containing the this value of the Entity', function () {
-            const entity = new Entity('10', new UserRepository());
-            sandbox.stub(Entity.prototype, '_load').returns(Promise.resolve(this._exists));
-            return expect(entity.load())
-                .to.eventually.be.fulfilled
-                .then((data) => {
-                    expect(data).to.deep.equal(entity);
-                });
-        });
-
-        it('rejects a promise with Resource not found HTTP 404', function () {
-            this._exists = false;
-            const entity = new Entity('10', new UserRepository());
-            sandbox.stub(Entity.prototype, '_load').returns(Promise.resolve(this._exists));
-            return expect(entity.load())
-                .to.eventually.be.rejectedWith('Resource not found')
-                .that.has.property('statusCode')
-                .that.is.equal(404);
+  describe('load()', function () {
+    it('resolves a promise containing the this value of the Entity', function () {
+      const entity = new Entity('10', new UserRepository());
+      sandbox.stub(Entity.prototype, '_load').returns(Promise.resolve(this._exists));
+      return expect(entity.load())
+        .to.eventually.be.fulfilled
+        .then((data) => {
+          expect(data).to.deep.equal(entity);
         });
     });
 
-    describe('_load()', function () {
-        it('resolves a promise with true when user is found', function () {
-            const entity = new Entity('10', new UserRepository());
-            sandbox.stub(BaseRepository.prototype, 'getById')
-                .returns(Promise.resolve({ user: 'dummy user' }));
-            return expect(entity._load())
-                .to.eventually.be.fulfilled
-                .then((data) => {
-                    expect(data).to.equal(true);
-                    expect(entity._loaded).to.equal(true);
-                });
-        });
+    it('rejects a promise with Resource not found HTTP 404', function () {
+      this._exists = false;
+      const entity = new Entity('10', new UserRepository());
+      sandbox.stub(Entity.prototype, '_load').returns(Promise.resolve(this._exists));
+      return expect(entity.load())
+        .to.eventually.be.rejectedWith('Resource not found')
+        .that.has.property('statusCode')
+        .that.is.equal(404);
+    });
+  });
 
-        it('resolves a promise with false when user does not exist', function () {
-            const entity = new Entity('10', new UserRepository());
-            sandbox.stub(BaseRepository.prototype, 'getById')
-                .returns(Promise.resolve(null));
-            return expect(entity._load())
-                .to.eventually.be.fulfilled
-                .then((data) => {
-                    expect(data).to.equal(false);
-                });
-        });
-
-        it('merges the returned entity with its this value when _exists', function () {
-            const entity = new Entity('10', new UserRepository());
-            sandbox.stub(BaseRepository.prototype, 'getById')
-                .returns(Promise.resolve({ testUser: 'testData' }));
-            return expect(entity._load())
-                .to.eventually.be.fulfilled
-                .then(() => {
-                    expect(entity).to.have.property('testUser', 'testData');
-                });
+  describe('_load()', function () {
+    it('resolves a promise with true when user is found', function () {
+      const entity = new Entity('10', new UserRepository());
+      sandbox.stub(BaseRepository.prototype, 'getById')
+        .returns(Promise.resolve({ user: 'dummy user' }));
+      return expect(entity._load())
+        .to.eventually.be.fulfilled
+        .then((data) => {
+          expect(data).to.equal(true);
+          expect(entity._loaded).to.equal(true);
         });
     });
 
-    describe('_save()', function () {
-        it('resolves a promise containing the user returned from the repository', function () {
-            const entity = new Entity('10', new UserRepository());
-            sandbox.stub(BaseRepository.prototype, 'create')
-                .returns(Promise.resolve({
-                    testUserKey: 'testUserValue'
-                }));
-
-            return expect(entity._save())
-                .to.eventually.be.fulfilled
-                .then((data) => {
-                    expect(data).to.deep.equal({ testUserKey: 'testUserValue' })
-                })
-        });
-
-        it('rejects a promise containing the error caught from the repository', function () {
-            const entity = new Entity('10', new UserRepository());
-            sandbox.stub(BaseRepository.prototype, 'create')
-                .returns(Promise.reject(new Error('Some Error')));
-
-            return expect(entity._save())
-                .to.eventually.be.rejectedWith('Some Error');
+    it('resolves a promise with false when user does not exist', function () {
+      const entity = new Entity('10', new UserRepository());
+      sandbox.stub(BaseRepository.prototype, 'getById')
+        .returns(Promise.resolve(null));
+      return expect(entity._load())
+        .to.eventually.be.fulfilled
+        .then((data) => {
+          expect(data).to.equal(false);
         });
     });
+
+    it('merges the returned entity with its this value when _exists', function () {
+      const entity = new Entity('10', new UserRepository());
+      sandbox.stub(BaseRepository.prototype, 'getById')
+        .returns(Promise.resolve({ testUser: 'testData' }));
+      return expect(entity._load())
+        .to.eventually.be.fulfilled
+        .then(() => {
+          expect(entity).to.have.property('testUser', 'testData');
+        });
+    });
+  });
+
+  describe('_save()', function () {
+    it('resolves a promise containing the user returned from the repository', function () {
+      const entity = new Entity('10', new UserRepository());
+      sandbox.stub(BaseRepository.prototype, 'create')
+        .returns(Promise.resolve({
+          testUserKey: 'testUserValue'
+        }));
+
+      return expect(entity._save())
+        .to.eventually.be.fulfilled
+        .then((data) => {
+          expect(data).to.deep.equal({ testUserKey: 'testUserValue' })
+        })
+    });
+
+    it('rejects a promise containing the error caught from the repository', function () {
+      const entity = new Entity('10', new UserRepository());
+      sandbox.stub(BaseRepository.prototype, 'create')
+        .returns(Promise.reject(new Error('Some Error')));
+
+      return expect(entity._save())
+        .to.eventually.be.rejectedWith('Some Error');
+    });
+  });
 });
