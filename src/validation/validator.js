5fe6acd2f107c9ff40a162b89543abcfc7fbf9a9
Victor <victor.ricart@outlook.com>
2019-03-09 13:22:44 +0100

    Modify validator to accept custom error messages

--- src/validation/validator.js
+++ src/validation/validator.js
@@ -2,10 +2,16 @@ const common = require('commensal-common');
 const Joi = require('joi');
 
 class Validator {
-  static validate(request, schema) {
+  static validate(request, schema, customError) {
     return Joi.validate(request, schema, (err, value) => {
       if (err) {
-        throw new common.errors.HttpError(err.details[0].message, 400);
+        err.details.forEach((detail) => { // if custom error specified, override error message
+          if (customError && customError[detail.context.key]) {
+            const message = `${customError[detail.context.key]}. Key: ${detail.context.key}. Got: ${detail.context.value}`;
+            throw new common.errors.HttpError(message, 400);
+          }
+          throw new common.errors.HttpError(err.details[0].message, 400);
+        });
       }
       return value;
     });
